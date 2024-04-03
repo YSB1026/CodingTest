14ef6ffe74f87f15528d295edda968cf5193957e
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2024-04-03 16:47:58 +0900

    Update N으로 표현.cpp

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -7,13 +7,14 @@ const int INF = 987654321;
 
 int solution(int N, int number) {
     vector<unordered_set<int>> dp(9);
-
+    
+    int cur = 0;
+    for (int i = 1; i <9; i++) {
+        cur = cur * 10 + N;
+        dp[i].insert(cur);
+    }
+    
     for (int i = 1; i <= 8; i++) {
-        int cur = 0;
-        for (int j = 0; j <i; j++) {
-            cur = cur * 10 + N;
-            dp[i].insert(cur);
-        }
         for (int j = 1; j < i; j++) {
             int k = i - j;
             for (int x : dp[j]) {
