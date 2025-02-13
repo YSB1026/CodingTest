e8a781b8ca944764b0a066c28daf5e8f0c995b89
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-02-13 14:33:37 +0900

    Update 유연근무제.cpp

--- 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
@@ -1,24 +1,29 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
+
+int convertTime(int n) {
+    int h = n / 100;
+    int m = n % 100;
+    return h * 60 + m;
+}
 
 int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) {
     int answer = 0;
     
     for(int i=0; i<timelogs.size(); i++){
-        int hope_time = schedules[i];
+        int hope_time = convertTime(schedules[i]);
         int day = startday;
         bool isGetAward = true;
-        cout << "직원 " << i+1 <<"\n";
+
         for(int j=0; j<7; j++){
             //주중에 지각한경우
-            if(day<6 && timelogs[i][j] > hope_time+10){
+            if(day<6 && convertTime(timelogs[i][j]) > hope_time+10){
                 isGetAward = false;
                 break;
             }
-            cout << day <<"요일 " << timelogs[i][j] - hope_time <<"분 \n";
+            
             day = (day%7)+1;
         }
         if(isGetAward) answer++;
