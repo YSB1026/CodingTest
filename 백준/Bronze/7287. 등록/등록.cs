232fe6041cfc970f38ed9d1cbec3b7d239dbafde
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-03-04 17:36:47 +0900

    [Bronze V] Title: 등록, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace 백준7287_등록
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("76\ne10013");
+        }
+    }
+}
