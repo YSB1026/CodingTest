de5bdae80d1bf0a8556e4cb82a5b0d72220939a4
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-03-04 17:27:26 +0900

    [Bronze V] Title: Hello World, Time: 44 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace 백준2557_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
