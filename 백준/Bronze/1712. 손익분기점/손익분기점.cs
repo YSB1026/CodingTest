9f509360116417bcdd87674b4d3473978440cbc7
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-03-05 17:28:10 +0900

    [Bronze II] Title: 손익분기점, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.cs
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cs
@@ -10,7 +10,7 @@ namespace 백준1712_손익분기점
             //총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다.
             //최초로 총 수입이 총 비용보다 많아져 이익이 발생하는
             //지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
-
+            //c++로 풀때 왜이렇게 어렵게풀었노;
             int a, b, c;
             string[] input = Console.ReadLine().Split(' ');
 
@@ -22,8 +22,7 @@ namespace 백준1712_손익분기점
             if (b>=c) BREAK_EVEN_POINT = -1;
             else
             {
-                decimal _ = a/Math.Abs(b-c) + 1;
-                BREAK_EVEN_POINT = (int)Math.Ceiling(_);
+                BREAK_EVEN_POINT = a/Math.Abs(b-c) + 1;
             }
 
             Console.WriteLine(BREAK_EVEN_POINT);
