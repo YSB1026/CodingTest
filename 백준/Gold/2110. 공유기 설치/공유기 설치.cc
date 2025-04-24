f5024b0d73c2916e4a4e4586279969486d2773f4
YSB1026 <103161220+YSB1026@users.noreply.github.com>
2025-04-24 18:04:51 +0900

    [Gold IV] Title: 공유기 설치, Time: 112 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool canPlace(const vector<int>& houses, int distance, int c) {
+    int count = 1;
+    int last = houses[0];
+
+    for (int i = 1; i < houses.size(); ++i) {
+        if (houses[i] - last >= distance) {
+            count++;
+            last = houses[i];
+        }
+    }
+
+    return count >= c;
+}
+
+int solution()
+{
+    int n, c;
+    cin >> n >> c;
+
+    vector<int> houses(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> houses[i];
+    }
+
+    sort(houses.begin(), houses.end());
+
+    //BS
+    int left = 1;
+    int right = houses[n - 1] - houses[0];
+    int answer = 0;
+
+    while (left <= right) {
+        int mid = (left + right) / 2; // 설치 거리
+
+        if (canPlace(houses, mid, c)) {
+            answer = mid;
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    cout << answer << endl;
+    return 0;
+}
+
+int main() {
+    return solution();
+}
+
